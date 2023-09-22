bd32ce039ef80b1bd100f7e22c1af27682abddd6
alexm92150 <145431029+alexm92150@users.noreply.github.com>
2023-09-22 11:46:51 +0200

    Add files via upload

---
+++ js/scriptboucle.js
@@ -0,0 +1,32 @@
+"use strict";
+
+//fait une boucle de 1 à 10 qui spam bonjour1, bonjour2, etc jusqu'à bonjour10
+for(let i=1;i<=10;i++){
+    console.log("bonjour n°"+i);
+}
+console.log(" ");
+console.log(" ");
+
+
+for(let i=10;i>=0;i--){
+    if(i==0){
+    alert("Hellooooo !");}
+
+    else{console.log(i);}
+}
+
+console.log(" ");
+console.log(" ");
+
+
+for(let i=10;i>=0;i--){
+    if(i!=0){ //  i!=0 egal i different de 0
+    console.log(i);}
+
+    else{console.log("Hellooooo le sangg!");}
+}
+
+let num=10;
+for(let i=0;i<num;i++){
+    console.log('OKLM');
+}
